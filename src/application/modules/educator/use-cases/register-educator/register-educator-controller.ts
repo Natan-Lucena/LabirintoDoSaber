0308a2b7682d73295aa5303376a8a5a728dd534d
Natan Lucena <zerogamerptbr@gmail.com>
2025-10-15 23:05:22 -0300

    :sparkles: feat: add register educator controller

---
+++ src/application/modules/educator/use-cases/register-educator/register-educator-controller.ts
@@ -0,0 +1,34 @@
+import {
+  BaseController,
+  formatValidationErrors,
+} from "@wave-telecom/framework/controllers";
+import { Request, Response } from "express";
+import { registerEducatorSchema } from "../../schemas/register-educator-schema";
+import { RegisterEducatorUseCase } from "./register-educator-use-case";
+
+export class RegisterEducatorController extends BaseController {
+  constructor(private useCase: RegisterEducatorUseCase) {
+    super();
+  }
+
+  async executeImpl(req: Request, res: Response): Promise<Response> {
+    const validation = await registerEducatorSchema.safeParseAsync(req.body);
+
+    if (!validation.success) {
+      const errors = formatValidationErrors(validation.error);
+      return this.clientError(res, undefined, errors);
+    }
+
+    const payload = validation.data;
+
+    const result = await this.useCase.execute(payload);
+
+    if (!result.ok) {
+      if (result.error == "EDUCATOR_ALREADY_EXISTS") {
+        return this.clientError(res, "EDUCATOR_ALREADY_EXISTS");
+      }
+    }
+
+    return this.created(res);
+  }
+}
