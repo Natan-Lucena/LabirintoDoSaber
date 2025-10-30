6359117aa137533fb1433dc666dbf65e3a2bb688
harielfr <enzoharielferreira@gmail.com>
2025-10-29 23:39:57 -0300

    feat(tasks): criar rota POST /create com controller e use-case

---
+++ src/application/modules/tasks/routes/index.ts
@@ -0,0 +1,15 @@
+import { Request, Response, Router } from "express";
+import { MockTaskRepository } from "../../../../infraestructure/repositories/mock/task-repository-impl";
+import { CreateTaskUseCase} from "../use-cases/create-task-use-case";
+import { CreateTaskController } from "../use-cases/create-task-controller";
+
+const tasksRouter = Router();
+
+const taskRepository = new MockTaskRepository();
+const createTaskUseCase = new CreateTaskUseCase(taskRepository);
+
+tasksRouter.post("/create", (req: Request, res: Response) => {
+    new CreateTaskController(createTaskUseCase).execute(req, res);
+});
+
+export { tasksRouter };
